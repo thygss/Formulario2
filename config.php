50b68b78b2b28632c02672b651b244544c0d83d2
Thiago Rocha <73665032+thygss@users.noreply.github.com>
2023-02-05 12:49:19 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'formularios';
+    
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+?>
